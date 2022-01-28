892da007a253136681bfb06944359351d2ae56d6
nseyoum <nseyoum@ucsd.edu>
2022-01-27 19:13:52 -0800

    debugged test-file6

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,6 +24,9 @@ public class MarkdownParse {
             if(openParen == -1 || closeParen == -1 || nextOpenBracket == - 1 || nextCloseBracket == -1){
                 return toReturn;
             }
+            if(markdown.substring(nextOpenBracket-1,nextOpenBracket).equals("!")){
+                return toReturn;
+            }
             if(openParen - nextCloseBracket <= 1){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
