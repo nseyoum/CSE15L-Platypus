d317d83f378f12b15910fc442324a17cd16764b2
nseyoum <nseyoum@ucsd.edu>
2022-02-03 19:44:12 -0800

    change

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -65,6 +65,7 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         ArrayList<String> expected = new ArrayList<String>();
         expected.add("page1.com");
+        expected.add("page1.com");
         assertEquals(expected,links);
     }
 
